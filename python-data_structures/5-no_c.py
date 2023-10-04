e09d7cb1314c9d3444e996ad6d86a7a19d82b8bb
Frech333 <josefrech@gmail.com>
2023-10-04 14:32:42 -0600

    Changes

--- python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-#hayo
-def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
-    return("".join(listofchars))
