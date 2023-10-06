935427d713fb5e4674969cf7e33d52658ea0569a
Frech333 <josefrech@gmail.com>
2023-10-06 12:22:19 -0600

    Python more data structures

--- python-more_data_structures/8-simply_delete.py
+++ python-more_data_structures/8-simply_delete.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary is None:
-        return
-    if key in a_dictionary:
-        del a_dictionary[key]
+    a_dictionary.pop(key, None)
     return a_dictionary
