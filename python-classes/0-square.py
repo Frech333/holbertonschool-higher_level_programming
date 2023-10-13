575e1b0c82d6d3550ce6f78e147d0e46684b51d3
Frech333 <josefrech@gmail.com>
2023-10-12 20:03:44 -0600

    Python Classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Define the class square
+class Square:
+      def __init__(self, side_lenght):
+            self.side_lenght = side_lenght
+def main():
+      aSquare = Square(5)
+
+if __name__ == "__main__":
+      main()
