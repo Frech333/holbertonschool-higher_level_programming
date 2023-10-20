84333ace2b681d71db0a830d977af0a449ff9f03
Frech333 <josefrech@gmail.com>
2023-10-20 12:14:07 -0600

    Python inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
+"""Defines available attributes/methods of an object."""
+
 def lookup(obj):
-    """
-    Returns a list of available attributes and methods of an object.
-
-    :param obj: The object to inspect.
-    :return: A list of attribute and method names.
-    """
-    return [name for name in dir(obj)]
-
-# Example usage:
-class Example:
-    def __init__(self):
-        self.name = "Example"
-    
-    def say_hello(self):
-        print("Hello!")
-
-obj = Example()
-attributes_and_methods = lookup(obj)
-print(attributes_and_methods)
+    """Return a list of available attributes."""
+    return (dir(obj))
